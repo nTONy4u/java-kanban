f3188ba6a7def8ea23d0233cdf0e01514b43abcb
Anton Chuprakov <t0ny4u@yandex.ru>
2025-06-11 12:22:21 +0300

    Fix checkstyle errors in InMemoryTaskManager

--- src/fun/ntony4u/kanban/service/InMemoryTaskManager.java
+++ src/fun/ntony4u/kanban/service/InMemoryTaskManager.java
@@ -5,7 +5,6 @@ import fun.ntony4u.kanban.model.*;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.TreeSet;
 import java.util.Comparator;
